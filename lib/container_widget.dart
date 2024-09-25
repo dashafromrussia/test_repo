129ac8b7b5107d6cb384ea672958fb0646e7bb6b
Daria Kurzeneva <dariakurzeneva@MacBook-Pro-Daria.local>
2024-09-25 12:51:45 +0300

    container widget created

---
+++ lib/container_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContainerWidget extends StatefulWidget {
+  const ContainerWidget({super.key});
+
+  @override
+  State<ContainerWidget> createState() => _ContainerWidgetState();
+}
+
+class _ContainerWidgetState extends State<ContainerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
